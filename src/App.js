1a9e98c7c32a1ef4d9c48bcf2631c5d05d6d6a4f
Daniele Di Salvo <disi91@hotmail.it>
2023-04-10 16:56:04 +0200

    Update App.js, package.json

--- src/App.js
+++ src/App.js
@@ -1,135 +1,141 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Layout, {LeftCol, RightCol} from "./components/Layout";
 import ListNames from "./components/ListNames";
 import User from "./components/User";
-import {v4 as uuid} from "uuid";
 import {NoListView} from "./components/NoListView";
 import ListView from "./components/ListView";
 import {NewListButton} from "./components/NewListButton";
-
-const user = {
-    id: 1,
-    name: "Daniele",
-    image: "https://github.com/lifeisfoo.png",
-};
-
-const initialLists = [
-    {id: 1, name: "Importante", undone_count: 0},
-    {id: 2, name: "Film da vedere", undone_count: 2},
-    {id: 3, name: "Libri da leggere", undone_count: 0},
-];
-
-const initialTodos = [
-    {listId: 2, id: 1, done: false, text: "Prima attività"},
-    {listId: 2, id: 2, done: true, text: "Seconda attività"},
-    {listId: 2, id: 3, done: false, text: "Terza attività"},
-];
+import {deleteData, getData, patchData, postData} from "./utils";
+import ReactModal from "react-modal";
+import ErrorModal from "./components/ErrorModal";
 
 export default function App() {
-    const [allLists, setAllLists] = useState(initialLists);
-    const [allTodos, setAllTodos] = useState(initialTodos);
+    const [user, setUser] = useState({})
+    const [lists, setLists] = useState([]);
     const [listIdx, setListIdx] = useState(-1);
     const [todos, setTodos] = useState([]);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        getData("/api/user").then((retUser) => setUser(retUser));
+        getData("/api/lists").then((retLists) => setLists(retLists));
+    }, []);
 
     const selectListByIdx = (idx) => {
         setListIdx(idx);
-        setTodos(allTodos.filter((t) => t.listId === allLists[idx].id));
+        const listId = lists[idx].id;
+        getData(`/api/todos?listId=${listId}`).then((retTodos) => {
+            setTodos(retTodos);
+        });
     };
 
     const addToListCount = (listIdx, num) => {
-        const tmpLists = [...allLists];
+        const tmpLists = [...lists];
         tmpLists[listIdx] = {...tmpLists[listIdx]};
         tmpLists[listIdx].undone_count += num;
-        setAllLists(tmpLists);
+        setLists(tmpLists);
     };
 
     const handleCreateTodo = (text) => {
-        const newTodo = {
-            listId: allLists[listIdx].id,
-            id: uuid(),
-            done: false,
-            text: text,
-        };
-        setAllTodos([...allTodos, newTodo]);
-        setTodos([...todos, newTodo]);
-        addToListCount(listIdx, 1);
+        const listId = lists[listIdx].id;
+        postData(`/api/todos`, {listId, text, done: false})
+            .then((newTodo) => {
+                setTodos([newTodo, ...todos]);
+                addToListCount(listIdx, 1);
+            })
+            .catch((e) =>
+                setError(`Errore durante la creazione dell'attività: ${e.status}`)
+            );
     };
 
     const handleUpdateTodo = (id, data) => {
-        const todoIdx = allTodos.findIndex((t) => t.id === id);
-        const preTodo = allTodos[todoIdx];
-        const updatedTodo = {
-            ...preTodo,
-            ...data,
-        };
+        const todoIdx = todos.findIndex((t) => t.id === id);
+        const preTodo = todos[todoIdx];
+        patchData(`/api/todos/${id}`, data).then((patchedTodo) => {
+            const tmpTodos = [...todos];
+            tmpTodos[todoIdx] = patchedTodo;
+            setTodos(tmpTodos.filter((t) => t.listId === patchedTodo.listId));
 
-        const tmpTodos = [...allTodos];
-        tmpTodos[todoIdx] = updatedTodo;
-        setAllTodos(tmpTodos);
-        setTodos(tmpTodos.filter((t) => t.listId === updatedTodo.listId));
-
-        const isTodoStatusChanged = preTodo.done !== updatedTodo.done;
-        if (isTodoStatusChanged) {
-            addToListCount(listIdx, preTodo.done ? 1 : -1);
-        }
+            const isTodoStatusChanged = preTodo.done !== patchedTodo.done;
+            if (isTodoStatusChanged) {
+                const listIdx = lists.findIndex((l) => l.id === patchedTodo.listId);
+                addToListCount(listIdx, preTodo.done ? 1 : -1);
+            }
+        });
     };
 
     const handleDeleteTodo = (id) => {
-        const todoIdx = todos.findIndex((t) => t.id === id);
-        const todo = todos[todoIdx];
-        const tmpTodos = [...todos];
-        tmpTodos.splice(todoIdx, 1);
-        setTodos(tmpTodos);
-        addToListCount(listIdx, todo.done ? 0 : -1);
+        deleteData(`/api/todos/${id}`).then((deletedTodo) => {
+            const todoIdx = todos.findIndex((t) => t.id === id);
+            const todo = todos[todoIdx];
+            const tmpTodos = [...todos];
+            tmpTodos.splice(todoIdx, 1);
+            setTodos(tmpTodos);
+            addToListCount(listIdx, todo.done ? 0 : -1);
+        });
     };
 
     const handleCreateList = () => {
-        const newList = {id: uuid(), name: "Nuovo elenco", undone_count: 0};
-        setAllLists([...allLists, newList]);
-        setListIdx(allLists.length);
+        postData("/api/lists", {name: "Nuovo elenco"}).then((newList) => {
+            setLists([...lists, newList]);
+            setListIdx(lists.length);
+            setTodos([]);
+        });
     };
 
     const handleDeleteList = (id) => {
-        const listIdx = allLists.findIndex((l) => l.id === id);
-        const tmpLists = [...allLists];
-        tmpLists.splice(listIdx, 1);
-        setAllLists(tmpLists);
-        setListIdx(-1);
+        deleteData(`/api/lists/${id}`).then((deletedList) => {
+            const listIdx = lists.findIndex((l) => l.id === id);
+            const tmpLists = [...lists];
+            tmpLists.splice(listIdx, 1);
+            setLists(tmpLists);
+            setListIdx(-1);
+        });
     };
 
     const handleUpdateListName = (id, name) => {
-        const listIdx = allLists.findIndex((l) => l.id === id);
-        const tmpLists = [...allLists];
-        tmpLists[listIdx].name = name;
-        setAllLists(tmpLists);
+        patchData(`/api/lists/${id}`, {name}).then((patchedList) => {
+            const listIdx = lists.findIndex((l) => l.id === id);
+            const tmpLists = [...lists];
+            tmpLists[listIdx] = patchedList;
+            setLists(tmpLists);
+        });
     };
 
     return (
-        <Layout>
-            <LeftCol>
-                <User name={user.name} image={user.image}>
-                    <NewListButton onCreateList={handleCreateList}/>
-                </User>
-                <hr/>
-                <ListNames
-                    lists={allLists}
-                    selectedListIdx={listIdx}
-                    onListClick={selectListByIdx}/>
-            </LeftCol>
-            <RightCol>
-                {listIdx === -1 ? (<NoListView/>) : (
-                    <ListView
-                        list={allLists[listIdx]}
-                        todos={todos}
-                        onTodoCreate={handleCreateTodo}
-                        onTodoDelete={handleDeleteTodo}
-                        onTodoUpdate={handleUpdateTodo}
-                        onListDelete={handleDeleteList}
-                        onListNameUpdate={handleUpdateListName}
+        <>
+            <ReactModal isOpen={Boolean(error)}>
+                <ErrorModal message={error} onConfirm={() => setError(false)}/>
+            </ReactModal>
+            <Layout>
+                <LeftCol>
+                    <User name={user.name} image={user.image}>
+                        <NewListButton onCreateList={handleCreateList}/>
+                    </User>
+                    <hr/>
+                    <ListNames
+                        lists={lists}
+                        selectedListIdx={listIdx}
+                        onListClick={selectListByIdx}
                     />
-                )}
-            </RightCol>
-        </Layout>
+                </LeftCol>
+                <RightCol>
+                    {listIdx === -1 ? (
+                        <NoListView/>
+                    ) : (
+                        <ListView
+                            list={lists[listIdx]}
+                            todos={todos}
+                            onTodoCreate={handleCreateTodo}
+                            onTodoDelete={handleDeleteTodo}
+                            onTodoUpdate={handleUpdateTodo}
+                            onListDelete={handleDeleteList}
+                            onListNameUpdate={handleUpdateListName}
+                        />
+                    )}
+                </RightCol>
+            </Layout>
+        </>
     );
 }
 
